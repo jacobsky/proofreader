ccf4b41d22cfbfc7dfbcd363bcfe9c161cf65408
Jacobsky <jacobsky@users.noreply.github.com>
2025-10-28 19:58:05 +0900

    Added proper error messaging to the user for common bad requests
    Signed-off-by: Jacobsky <jacobsky@users.noreply.github.com>

--- internal/web/components/home/home.go
+++ internal/web/components/home/home.go
@@ -56,16 +56,57 @@ type AIAPISignal struct {
 	Prompt   string `json:"prompt"`
 }
 
+func (signal *AIAPISignal) validate() error {
+	errors := make([]string, 0)
+	if signal.Model == "" {
+		errors = append(errors, "Model is not set. Please set it in the configuration")
+	}
+	if signal.Endpoint == "" {
+		errors = append(errors, "Endpoint is not set. Please set it in the configuration")
+	}
+	if signal.Key == "" {
+		errors = append(errors, "Key is not set. Please set it in the configuration")
+	}
+	if signal.Prompt == "" {
+		errors = append(errors, "Prompt is empty. Please enter a message to submit for inference")
+	}
+	if len(errors) == 0 {
+		return nil
+	} else {
+		return fmt.Errorf("validation errors\n %v", strings.Join(errors, "\n"))
+	}
+}
+
+func handleDatastarError(sse *datastar.ServerSentEventGenerator, w http.ResponseWriter, status int, err error) {
+	// Base case, if no error to handle, skip over it.
+	slog.Debug("Error", "status", status, "error-msg", err)
+	if err != nil {
+		err := sse.PatchElementTempl(OutputSection(ErrorMessage(status, err)))
+		if err != nil {
+			slog.Error("Error", "status", status, "error-msg", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+}
+
 func proofread(w http.ResponseWriter, r *http.Request) {
 	var store = &AIAPISignal{}
-	if err := datastar.ReadSignals(r, store); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+
+	err := datastar.ReadSignals(r, store) // sse.PatchSignals([]byte(`{fetching: true}`))
+
+	sse := datastar.NewSSE(w, r)
+	if err != nil {
+		handleDatastarError(sse, w, http.StatusInternalServerError, err)
 		return
 	}
-	sse := datastar.NewSSE(w, r)
-	// sse.PatchSignals([]byte(`{fetching: true}`))
 
 	slog.Info(fmt.Sprintf("Received %v, %v, %v, %v", store.Endpoint, store.Model, store.Key, store.Prompt))
+	err = store.validate()
+
+	if err != nil {
+		handleDatastarError(sse, w, http.StatusBadRequest, err)
+		return
+	}
 
 	llm, err := openai.New(
 		openai.WithModel(store.Model),
@@ -73,19 +114,19 @@ func proofread(w http.ResponseWriter, r *http.Request) {
 		openai.WithToken(store.Key),
 	)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		handleDatastarError(sse, w, http.StatusBadRequest, err)
 		return
 	}
 	err = sse.PatchElementTempl(OutputSection(WaitingForInput()))
 
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	err = sse.PatchSignals([]byte(`{ prompt_output: "Waiting for input..."}`))
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	output := ""
@@ -123,14 +164,13 @@ func proofread(w http.ResponseWriter, r *http.Request) {
 			return sse.PatchSignals([]byte(outputsignal))
 		}))
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		handleDatastarError(sse, w, http.StatusInternalServerError, err)
 		return
 	}
 
 	var buf bytes.Buffer
 	if err := md.Convert([]byte(resp.Choices[0].Content), &buf); err != nil {
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		slog.Error("Markdown parsing error", "error", err)
+		handleDatastarError(sse, w, http.StatusInternalServerError, fmt.Errorf("an internal error has occurred"))
 	}
 	htmlcontent := unsafeRenderMarkdown(buf.String())
 	// sse.PatchSignals([]byte(`{fetching: false}`))
